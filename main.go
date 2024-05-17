7e2b1531aeb8d76dd16444d5fec5d94d887f8291
Teodor Janez Podobnik <teo.podobnik1234@gmail.com>
2024-05-17 23:14:32 +0000

    finished understanding

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"os"
+	"io"
 	"fmt"
 	"log"
 	"net"
@@ -19,26 +20,33 @@ import (
 )
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
-	log.Println("Hello called!")
+	io.WriteString(w, fmt.Sprintf("Hello from the %s target!\n", os.Args[1]))
 }
 
+// Inspired by src/net/dial.go
 func getListenConfig(prog *ebpf.Program, mode string, otherInstancesRunning bool) net.ListenConfig {
 	lc := net.ListenConfig{Control: func(network, address string, c syscall.RawConn) error {
 		var opErr error
+		// If Control is not nil, it is called after creating the network
+		// connection but before binding it to the operating system.
 		err := c.Control(func(fd uintptr) {
-			// Set SO_REUSEPORT on the socket
+			// Set SO_REUSEPORT on the socket for both instances (because eBPF program works on socket with SO_REUSEPORT configured)
+			// This sets the SO_REUSEPORT option on the socket, which allows multiple sockets to bind to the same port.
+			// In "function" words, for fd on the SOL_SOCKET level, set the SO_REUSEPORT option to 1 (a.k.a. true/on).
 			opErr = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
 			// Set eBPF program to be invoked for socket selection
 			if prog != nil && mode == "primary" && !otherInstancesRunning {
+				// SO_ATTACH_REUSEPORT_EBPF program defines how packets are assigned to the sockets in the reuseport group 
+				// That is, all sockets which have SO_REUSEPORT set and are using the same local address to receive packets.
+				// In "function" words, for fd on the SOL_SOCKET lever, set the unix.SO_ATTACH_REUSEPORT_EBPF option to eBPF program file descriptor.
 				err := unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_ATTACH_REUSEPORT_EBPF, prog.FD())
 				if err != nil {
 					opErr = fmt.Errorf("setsockopt(SO_ATTACH_REUSEPORT_EBPF) failed: %w", err)
 				} else {
-					log.Println("SO_ATTACH_REUSEPORT_EBPF completed successfully")
+					log.Println("eBPF program attached to the SO_REUSEPORT socket group!")
 				}
 			}
-		})
-		if err != nil {
+		}); if err != nil {
 			return err
 		}
 		return opErr
@@ -56,25 +64,28 @@ func GetFdFromListener(l net.Listener) int {
 }
 
 func main() {
-	// Remove resource limits for kernels <5.11.
-	if err := rlimit.RemoveMemlock(); err != nil { 
-		log.Print("Removing memlock:", err)
-	}
-
-	// Load the compiled eBPF ELF and load it into the kernel.
-	var objs reuseportlbObjects 
-	if err := loadReuseportlbObjects(&objs, nil); err != nil {
-			log.Print("Loading eBPF objects:", err)
-	}
-	defer objs.Close() 
-
 	mode := os.Args[1]
 	if mode != "primary" && mode != "standby" {
 		log.Println("Server mode should either be primary or standy")
 		return
 	}
 
-	// Check if other instances are running on the same port - because we are testing SO_REUSEPORT
+	// Remove resource limits for kernels <5.11.
+	if err := rlimit.RemoveMemlock(); err != nil { 
+		log.Print("Removing memlock:", err)
+	}
+
+	// Load the compiled eBPF ELF and load it into the kernel.
+	// Map needs to be pinned, such that in case the primary target is shutdown, the standby target can still see the map
+	var objs reuseportlbObjects 
+	if mode == "primary" {
+		if err := loadReuseportlbObjects(&objs, &ebpf.CollectionOptions{Maps: ebpf.MapOptions{PinPath: "/sys/fs/bpf/tc/globals"}}); err != nil {
+				log.Print("Loading eBPF objects:", err)
+		}
+	}
+	defer objs.Close() // This only unloads the eBPF program (if it is not attached to kernel) and map, but doesn't remove the pin
+
+	// Check if other instances are already running on the same port - because we are testing SO_REUSEPORT
 	fs, _ := procfs.NewDefaultFS()
 	netTCP, _ := fs.NetTCP()
 	otherInstancesRunning := false
@@ -85,6 +96,8 @@ func main() {
 		}
 	}
 
+	// Setup HTTP Server instance
+	// We can't directly use http.ListenAndServe because it hides the socket implementation (which is what we are interested in with SetsockoptInt)
 	http.HandleFunc("/hello", handleHello)
 	server := http.Server{Addr: "127.0.0.1:8080", Handler: nil}
 	lc := getListenConfig(objs.reuseportlbPrograms.HotStandbySelector, mode, otherInstancesRunning)
@@ -95,7 +108,7 @@ func main() {
 		log.Println("Started listening in 127.0.0.1:8080 successfully !")
 	}
 
-	// Socket FD is the same for both instances - possible because of SO_REUSEPORT
+		// NOTE: Each process has it's own file descriptor table, so don't get confused if the FDs are the same for both processes
 	v := uint64(GetFdFromListener(ln))
 	var k uint32
 	if mode == "primary" {
@@ -103,9 +116,14 @@ func main() {
 	} else {
 		k = uint32(1)
 	}
-	log.Printf("Updating with k=%d v=%d", k, v)
 
-	err = objs.reuseportlbMaps.TcpBalancingTargets.Update(&k, &v, unix.BPF_ANY)
+	log.Printf("Updating with (key = %d , value = %d)", k, v)
+	m, err := ebpf.LoadPinnedMap("/sys/fs/bpf/tc/globals/tcp_balancing_targets", nil)
+	if err != nil {
+		log.Fatal("Unable to load map: %w", err)
+	}
+
+	err = m.Update(&k, &v, ebpf.UpdateAny)
 	if err != nil {
 		log.Fatal("Unable to update the map %w", err)
 	} else {
--- reuseportlb.c
+++ reuseportlb.c
@@ -8,9 +8,11 @@ struct {
 	__uint(max_entries, 128);
 	__type(key, __u32); // Either 0 or 1 in this example
 	__type(value, __u64); // Socket FD
+    __uint(pinning, LIBBPF_PIN_BY_NAME);
 } tcp_balancing_targets SEC(".maps");
 
-
+// This function is called for each incoming packet for the reuseport group, 
+// but only get's attached when we run primary program due to our user space logic
 SEC("sk_reuseport/selector")
 enum sk_action hot_standby_selector(struct sk_reuseport_md *reuse) {
     enum sk_action action;
@@ -20,10 +22,17 @@ enum sk_action hot_standby_selector(struct sk_reuseport_md *reuse) {
         return SK_DROP;
     }
 
+    bpf_printk("Selecting socket...");
     // Invoke kernel helper sk_select_reuseport to select socket by index from reuseport sockarray
+    // Select a SO_REUSEPORT socket from a BPF_MAP_TYPE_REUSEPORT_SOCKARRAY map.  
+    // It checks the selected socket is matching the incoming request in the socket buffer.
+    // In general it should match both sockets if they are present (listening), but the "primary" takes precedence, just because it is the first in the if statement.
+    // This is intentional, as we want to have a primary socket and a fallback socket for showcasing the hot standby.
     if (bpf_sk_select_reuseport(reuse, &tcp_balancing_targets, &built_in_key, 0) == 0) {
+        bpf_printk("Selected primary socket\n");
         action = SK_PASS;
     } else if (bpf_sk_select_reuseport(reuse, &tcp_balancing_targets, &fall_back_key, 0) == 0) {
+        bpf_printk("Selected fallback socket\n");
         action = SK_PASS;
     } else {
         action = SK_DROP;
